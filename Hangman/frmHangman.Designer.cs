275026b52d897315165b22742d3a959a5c8e7092
cemtaskin <cemtaskin82@gmail.com>
2024-02-26 21:27:16 +0300

    Hanhman Part 01

--- Hangman/frmHangman.Designer.cs
+++ Hangman/frmHangman.Designer.cs
@@ -30,34 +30,35 @@ namespace Hangman
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imgHangman = new System.Windows.Forms.PictureBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.lblDescription = new System.Windows.Forms.Label();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblKey = new System.Windows.Forms.Label();
+            this.grbLetters = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgHangman)).BeginInit();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.pictureBox1);
-            this.groupBox1.Location = new System.Drawing.Point(595, 12);
+            this.groupBox1.Controls.Add(this.imgHangman);
+            this.groupBox1.Location = new System.Drawing.Point(643, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(276, 544);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             // 
-            // pictureBox1
+            // imgHangman
             // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Image = global::Hangman.Properties.Resources._001;
-            this.pictureBox1.Location = new System.Drawing.Point(3, 16);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(270, 525);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.imgHangman.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imgHangman.Image = global::Hangman.Properties.Resources._001;
+            this.imgHangman.Location = new System.Drawing.Point(3, 16);
+            this.imgHangman.Name = "imgHangman";
+            this.imgHangman.Size = new System.Drawing.Size(270, 525);
+            this.imgHangman.TabIndex = 0;
+            this.imgHangman.TabStop = false;
             // 
             // groupBox2
             // 
@@ -80,38 +81,49 @@ namespace Hangman
             // 
             // groupBox3
             // 
-            this.groupBox3.Location = new System.Drawing.Point(18, 600);
+            this.groupBox3.Location = new System.Drawing.Point(15, 562);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(853, 100);
+            this.groupBox3.Size = new System.Drawing.Size(904, 100);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             // 
-            // label1
+            // lblKey
             // 
-            this.label1.BackColor = System.Drawing.Color.White;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(137, 186);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(281, 348);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "A";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblKey.BackColor = System.Drawing.Color.White;
+            this.lblKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKey.Location = new System.Drawing.Point(201, 183);
+            this.lblKey.Name = "lblKey";
+            this.lblKey.Size = new System.Drawing.Size(191, 210);
+            this.lblKey.TabIndex = 3;
+            this.lblKey.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // grbLetters
+            // 
+            this.grbLetters.Location = new System.Drawing.Point(18, 422);
+            this.grbLetters.Name = "grbLetters";
+            this.grbLetters.Size = new System.Drawing.Size(619, 134);
+            this.grbLetters.TabIndex = 4;
+            this.grbLetters.TabStop = false;
             // 
             // frmHangman
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(883, 732);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(926, 732);
+            this.Controls.Add(this.grbLetters);
+            this.Controls.Add(this.lblKey);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.KeyPreview = true;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmHangman";
             this.Text = "Hangman";
+            this.Load += new System.EventHandler(this.frmHangman_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.frmHangman_KeyPress);
             this.groupBox1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgHangman)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -120,11 +132,12 @@ namespace Hangman
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox imgHangman;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Label lblDescription;
         private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblKey;
+        private System.Windows.Forms.GroupBox grbLetters;
     }
 }
 
--- Hangman/frmHangman.cs
+++ Hangman/frmHangman.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Hangman.Properties;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,84 @@ namespace Hangman
 {
     public partial class frmHangman : Form
     {
+        public List<String> UsedLetters { get; set; }
+        String chars = "ABCDEFGHIJKLMNOPRSTQWXYZ";
+        
+
         public frmHangman()
         {
             InitializeComponent();
+            UsedLetters = new List<string>();
+        }
+
+        private void frmHangman_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsLetter(e.KeyChar))
+            {
+                e.Handled = false;
+                return;
+            }
+            controlChar(e.KeyChar.ToString().ToUpper());
+            
+        }
+        private void controlChar(String letter)
+        {
+            if (!UsedLetters.Contains(letter))
+            {
+                lblKey.Text = letter; 
+                UsedLetters.Add(letter);
+                ResourceManager rm = Resources.ResourceManager; 
+                if (UsedLetters.Count != 0)
+                {
+                    Bitmap hangmanPicture = (Bitmap)rm.GetObject("_" + (UsedLetters.Count+1).ToString("000"));
+                    imgHangman.Image = hangmanPicture;
+                }
+                    
+                addLetters();
+            }
+        }
+
+        private void addLetters()
+        {
+            grbLetters.Controls.Clear();
+            int x = 0;
+            int y = 20;
+
+            for(int i=0;i<chars.Length;i++)
+            {
+                Button button = new Button();
+                button.Enabled = !UsedLetters.Contains(chars.Substring(i, 1));
+                
+                button.Text = chars.Substring(i, 1);
+                button.Width = 40;
+                button.Height = 40;
+                button.Top = y;
+                button.Left = (x) * 50 + 10 ;
+                x++;
+
+                if (i == 11)
+                {
+                    y = 80;
+                    x = 0;
+                }
+
+                button.Click += Button_Click;
+                grbLetters.Controls.Add(button);
+                    
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            controlChar(button.Text);
+            
+        }
+
+        private void frmHangman_Load(object sender, EventArgs e)
+        {
+            addLetters();
         }
     }
+
 }
